9f4cd9917718b18fa728f6ae210102c3a2fca53a
Mamamo <earl3jordan@gmail.com>
2024-09-16 01:45:30 +0800

    DanDaDan

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,37 +1,54 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Calculator extends JFrame {
     private JPanel Calculator;
-    private JLabel Output;
-    private JButton a7Button;
-    private JButton a8Button;
-    private JButton a9Button;
-    private JButton a4Button;
-    private JButton a5Button;
-    private JButton a6Button;
-    private JButton button7;
-    private JButton button8;
-    private JButton a1Button1;
-    private JButton a2Button1;
-    private JButton a3Button1;
-    private JButton button12;
-    private JButton a0Button;
-    private JButton πButton;
-    private JButton button1;
-    private JButton button2;
-    private JButton ACButton;
-    private JButton button3;
+    private JTextField Output;
+    private JButton no7;
+    private JButton no8;
+    private JButton no9;
+    private JButton no4;
+    private JButton no5;
+    private JButton no6;
+    private JButton plus;
+    private JButton minus;
+    private JButton no1;
+    private JButton no2;
+    private JButton no3;
+    private JButton divide;
+    private JButton no0;
+    private JButton pie;
+    private JButton dot;
+    private JButton multiply;
+    private JButton AC;
+    private JButton equal;
+    private JButton positivenegative;
 
     public Calculator() {
+
         setContentPane(Calculator);
         setTitle("Calculator");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(300,400);
+        setSize(400, 600);
+        setMinimumSize(new java.awt.Dimension(300, 400));
         setLocationRelativeTo(null);
         setVisible(true);
+
+        // Adding ActionListener to no1 button
+        no1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Output.setText(Output.getText() + "1");
+            }
+        });
     }
 
     public static void main(String[] args) {
         new Calculator();
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
