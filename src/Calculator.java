c2703d63e6ec40a78ea75a8c966249c4ddc22679
Edrian Evangelista <edrian.evangelista@gmail.com>
2024-09-16 10:02:11 +0800

    Button functions

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,22 +6,26 @@ public class Calculator extends JFrame {
     //Private UI elements
     private JPanel Calculator;
     private JTextField Output;
-    private JButton no7;
-    private JButton no8;
-    private JButton no9;
-    private JButton no4;
-    private JButton no5;
-    private JButton no6;
-    private JButton plus;
-    private JButton minus;
+    private JButton no0;
     private JButton no1;
     private JButton no2;
     private JButton no3;
+    private JButton no4;
+    private JButton no5;
+    private JButton no6;
+    private JButton no7;
+    private JButton no8;
+    private JButton no9;
+
+    private JButton plus;
+    private JButton minus;
     private JButton divide;
-    private JButton no0;
     private JButton multiply;
+
     private JButton AC;
     private JButton equal;
+
+    private JButton[] buttons = {no0, no1, no2, no3, no4, no5, no6, no7, no8, no9};
 
     public Calculator() {
 
@@ -33,17 +37,25 @@ public class Calculator extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
 
+        for (JButton button : buttons) {
+            System.out.println(button.getText());
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    Output.setText(Output.getText() + button.getText());
+                }
+            });
+        }
+
         // Adding ActionListener to buttons
-        no1.addActionListener(new ActionListener() {
+        AC.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Output.setText(Output.getText() + "1");
+                Output.setText("0");
             }
         });
 
-
     }
-
     public static void main(String[] args) {
         new Calculator();
     }
